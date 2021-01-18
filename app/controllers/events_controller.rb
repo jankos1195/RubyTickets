df40e46e30c4292af8fe58ec3490ff1564c71684
jankos1195 <jankos1195@gmail.com>
2021-01-18 01:58:37 +0100

    create event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,42 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
   
   def index
     @events = Event.all
   end
 
-  def new
+  def show
   end
 
-  def create
+  def new
+    @event = Event.new
   end
+ 
+  def create
+    @event = Event.new(event_params)
+ 
+    if @event.save
+      redirect_to "/events/#{@event.id}", notice: 'Event was successfully created.'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to tickets_url, notice: 'Ticket was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:artist, :description, :price_low, :price_high, :event_date)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'events/index'
+  get '/events'  => 'events#index', as: "events"
   get 'events/new'
-  get 'events/create'
+  post '/events' => 'events#create'
+  get 'events/:id' => 'events#show' 
   root :to => "tickets#index"
   resources :tickets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
