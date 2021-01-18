0e4a9660eb910f0f8264dff309b74a9be7f96d56
jankos1195 <jankos1195@gmail.com>
2021-01-18 02:11:35 +0100

    add start page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :check_logged_in, :only => [:new, :create]
   
   def index
     @events = Event.all
@@ -38,5 +39,11 @@ class EventsController < ApplicationController
 
     def event_params
       params.require(:event).permit(:artist, :description, :price_low, :price_high, :event_date)
+    end
+
+    def check_logged_in
+      authenticate_or_request_with_http_basic("Ads") do |username, password|
+      username == "admin" && password == "admin"
+      end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   get '/events'  => 'events#index', as: "events"
   get 'events/new'
   post '/events' => 'events#create'
-  get 'events/:id' => 'events#show' 
-  root :to => "tickets#index"
+  get 'events/:id' => 'events#show'
   resources :tickets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
